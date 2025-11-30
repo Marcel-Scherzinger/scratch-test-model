74ae5be41220919e1aa5fd18ee8dd963f553a330
Marcel Scherzinger <>
2025-11-30 12:05:41 +0100

    test: add test case for showing #2 is the problem and not #1
    It looks like boolean initial values cause problems

--- tests/mixed-variable-control-displays.rs
+++ tests/mixed-variable-control-displays.rs
@@ -9,3 +9,11 @@ fn parse_mixed_variable_control_displays_numeric() {
     }
 }
 
+#[test]
+fn parse_mixed_variable_control_displays_boolean() {
+    if let Err(err) =
+        ProjectDoc::from_sb3_file("sb3/mixed-variable-control-displays-boolean-values.sb3")
+    {
+        panic!("{err:#?}");
+    }
+}
