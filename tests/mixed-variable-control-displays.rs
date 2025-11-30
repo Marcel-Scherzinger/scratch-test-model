200e5f572d0b774dff2dafa409a6d8d97d214459
Marcel Scherzinger <>
2025-11-30 11:58:23 +0100

    test: add test case showing that #1 is not the real reason for the problem

---
+++ tests/mixed-variable-control-displays.rs
@@ -0,0 +1,11 @@
+use scratch_test_model::ProjectDoc;
+
+#[test]
+fn parse_mixed_variable_control_displays_numeric() {
+    if let Err(err) =
+        ProjectDoc::from_sb3_file("sb3/mixed-variable-control-displays-number-values.sb3")
+    {
+        panic!("{err:#?}");
+    }
+}
+
