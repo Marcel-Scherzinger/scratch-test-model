b338ac2b7ef0d9cf0b5da58f9256a5097d2b7546
Marcel Scherzinger <>
2026-03-08 17:49:08 +0100

    test(zip): add check for empty file list to folder traversal (#3)

--- tests/try-parsing-further.rs
+++ tests/try-parsing-further.rs
@@ -6,21 +6,21 @@ fn parse_in_folder(folder: &str) {
     let walker = walkdir::WalkDir::new(folder).into_iter();
     let files = walker.flatten().collect_vec();
 
-    let errors: Vec<_> = files
+    let (oks, errors): (Vec<_>, Vec<_>) = files
         .into_par_iter()
         .filter(|file| {
             file.file_type().is_file()
                 && file.path().extension().and_then(|s| s.to_str()) == Some("sb3")
         })
         .map(|file| {
-            let json = json_from_sb3_file(file.path()).unwrap();
+            let json = json_from_sb3_file(file.path())
+                .map_err(|err| (file.path().to_owned(), format!("{err:#?}")))?;
             ProjectDoc::from_json(&json)
                 .map(|_| ())
                 .map_err(|err| (file.path().to_owned(), format!("{err:#?}")))
-                .err()
         })
-        .flatten()
-        .collect();
+        .partition(Result::is_ok);
+    let errors: Vec<_> = errors.into_iter().flat_map(Result::err).collect();
 
     if !errors.is_empty() {
         let count = errors.len();
@@ -30,6 +30,9 @@ fn parse_in_folder(folder: &str) {
             .join("");
         panic!("Failed to parse {count} files:\n\n{out}");
     }
+    if oks.is_empty() {
+        panic!("No sb3 files found in folder: {folder:?}");
+    }
 }
 
 #[test]
@@ -37,23 +40,9 @@ fn try_parsing_further() {
     parse_in_folder("sb3/further/valid");
 }
 
-// As described in [https://github.com/Marcel-Scherzinger/scratch-test-model/issues/3]
-// this currently doesn't work
-//
-// It's marked as `should_panic` so it's noticed if a future version of a dependency fixes this
+// archives that failed to parse using other reading function (#3)
 #[test]
-#[should_panic]
-fn try_parsing_io_unsupported_archive() {
-    parse_in_folder("sb3/further/unsupported-archive");
-}
-#[test]
-fn try_parsing_zip() {
-    parse_in_folder("sb3/further/unsupported-archive/first.zip");
-    parse_in_folder("sb3/further/unsupported-archive/second.zip");
-}
-#[test]
-#[should_panic]
-fn try_parsing_sb3() {
-    parse_in_folder("sb3/further/unsupported-archive/first.sb3");
-    parse_in_folder("sb3/further/unsupported-archive/second.sb3");
+fn try_parsing_archives() {
+    parse_in_folder("sb3/archive/first.sb3");
+    parse_in_folder("sb3/archive/second.sb3");
 }
